1201f75ebfb7ac40866705efb6b40f847c5ec530
申浩然 <EchoRan6319@outlook.com>
2026-01-25 10:33:14 +0800

    chore: bump version to V2.0.0 RC

--- pages/SettingsPage.tsx
+++ pages/SettingsPage.tsx
@@ -157,7 +157,7 @@ const SettingsPage: React.FC<Props> = ({ settings, updateSettings }) => {
       </SettingSection>
 
       <div className="mt-8 text-center">
-        <p className="text-sm text-gray-400">SkyYou Weather v1.2.2 (PWA)</p>
+        <p className="text-sm text-gray-400">SkyYou Weather V2.0.0 RC (PWA)</p>
         <p className="text-xs text-gray-300 mt-1">Design inspired by Material You</p>
       </div>
     </div>
